c206b427cd601397b74fe5d6bc4786a0ea812d6f
Samuel Asije <asijesamuel88@gmail.com>
2026-05-18 20:12:05 +0100

    sam

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,66 @@
+// vite.config.ts
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+
+    tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true,
+      },
+
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+
+      manifest: {
+        name: "BIU Smart Hostel Key",
+        short_name: "Hostel Key",
+
+        description: "Smart QR Hostel Key Access System",
+
+        theme_color: "#111111",
+
+        background_color: "#111111",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        start_url: "/",
+
+        scope: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
